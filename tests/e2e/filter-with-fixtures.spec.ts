17dbae45375036965b16e7801a8bb9e413418638
Nick Dare <nick.dare@propeller.com.au>
2026-01-29 14:16:26 +1100

    fix: fixtures

--- tests/e2e/filter-with-fixtures.spec.ts
+++ tests/e2e/filter-with-fixtures.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from "@playwright/test";
-import { test } from "../fixtures/base";
+import { test } from "../fixtures/fixtures";
 
 const username = "demo";
 const password = "password123";
--- tests/fixtures/fixtures.ts
+++ tests/fixtures/fixtures.ts
@@ -1,5 +1,4 @@
 import { test as base } from "@playwright/test";
-
 import { LoginPage } from "../pages/login";
 import { TodosPage } from "../pages/todos";
 
@@ -10,11 +9,24 @@ export type TestOptions = {
 
 export const test = base.extend<TestOptions>({
   loginPage: async ({ page }, use) => {
+    // setup
     const login = new LoginPage(page);
+
+    // test runs
     await use(login);
+
+    // teardown
+    // any code we want to run after the test
   },
+
   todosPage: async ({ page }, use) => {
+    // setup
     const todos = new TodosPage(page);
+
+    // test runs
     await use(todos);
+
+    // teardown
+    // any code we want to run after the test
   },
 });
