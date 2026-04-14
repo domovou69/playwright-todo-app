215ea86cd275a40e5ce0b9734c3cf32be7439c10
mikhail.yesipchuk <domovou69@gmail.com>
2026-04-14 22:39:08 +0300

    yml with action.yml

--- tests/e2e/filter-with-beforeEach.spec.ts
+++ tests/e2e/filter-with-beforeEach.spec.ts
@@ -1,61 +1,43 @@
-import { expect } from "@playwright/test";
-import { test } from "../fixtures/fixtures";
+import { expect } from '@playwright/test'
+import { test } from '../fixtures/fixtures'
 
-const username = "demo";
-const password = "password123";
+const username = 'demo'
+const password = 'password123'
 
-test.describe("Filter Todos by Priority", () => {
+test.describe('Filter Todos by Priority', () => {
   test.beforeEach(async ({ page, loginPage }) => {
-    await page.goto("/");
+    await page.goto('/')
 
-    await loginPage.login(username, password);
-    await expect(page).toHaveURL("/todos");
-  });
-  test("Filter by low priority", async ({ todosPage }) => {
-    await todosPage.filters.selectPriority("Low");
-    await expect(todosPage.todoItem.filter({ hasText: "low" })).toBeVisible();
-    await expect(
-      todosPage.todoItem.filter({ hasText: "medium" }),
-    ).not.toBeVisible();
-    await expect(
-      todosPage.todoItem.filter({ hasText: "high" }),
-    ).not.toBeVisible();
-  });
+    await loginPage.login(username, password)
+    await expect(page).toHaveURL('/todos')
+  })
+  test('Filter by low priority', async ({ todosPage }) => {
+    await todosPage.filters.selectPriority('Low')
+    await expect(todosPage.todoItem.filter({ hasText: 'low' })).not.toBeVisible()
+    await expect(todosPage.todoItem.filter({ hasText: 'medium' })).not.toBeVisible()
+    await expect(todosPage.todoItem.filter({ hasText: 'high' })).not.toBeVisible()
+  })
 
-  test("Filter by medium priority", async ({ todosPage }) => {
-    await todosPage.filters.selectPriority("Medium");
-    await expect(
-      todosPage.todoItem.filter({ hasText: "low" }),
-    ).not.toBeVisible();
-    await expect(
-      todosPage.todoItem.filter({ hasText: "medium" }),
-    ).toBeVisible();
-    await expect(
-      todosPage.todoItem.filter({ hasText: "high" }),
-    ).not.toBeVisible();
-  });
+  test('Filter by medium priority', async ({ todosPage }) => {
+    await todosPage.filters.selectPriority('Medium')
+    await expect(todosPage.todoItem.filter({ hasText: 'low' })).toBeVisible()
+    await expect(todosPage.todoItem.filter({ hasText: 'medium' })).toBeVisible()
+    await expect(todosPage.todoItem.filter({ hasText: 'high' })).not.toBeVisible()
+  })
 
-  test("Filter by high priority", async ({ todosPage }) => {
-    await todosPage.filters.selectPriority("High");
-    await expect(
-      todosPage.todoItem.filter({ hasText: "low" }),
-    ).not.toBeVisible();
-    await expect(
-      todosPage.todoItem.filter({ hasText: "medium" }),
-    ).not.toBeVisible();
-    await expect(todosPage.todoItem.filter({ hasText: "high" })).toBeVisible();
-  });
+  test('Filter by high priority', async ({ todosPage }) => {
+    await todosPage.filters.selectPriority('High')
+    await expect(todosPage.todoItem.filter({ hasText: 'low' })).not.toBeVisible()
+    await expect(todosPage.todoItem.filter({ hasText: 'medium' })).not.toBeVisible()
+    await expect(todosPage.todoItem.filter({ hasText: 'high' })).toBeVisible()
+  })
 
-  test("Clear priority filter", async ({ todosPage }) => {
-    await todosPage.filters.selectPriority("Low");
-    await expect(
-      todosPage.todoItem.filter({ hasText: "high" }),
-    ).not.toBeVisible();
-    await todosPage.filters.clearFilters();
-    await expect(todosPage.todoItem.filter({ hasText: "low" })).toBeVisible();
-    await expect(
-      todosPage.todoItem.filter({ hasText: "medium" }),
-    ).toBeVisible();
-    await expect(todosPage.todoItem.filter({ hasText: "high" })).toBeVisible();
-  });
-});
+  test('Clear priority filter', async ({ todosPage }) => {
+    await todosPage.filters.selectPriority('Low')
+    await expect(todosPage.todoItem.filter({ hasText: 'high' })).not.toBeVisible()
+    await todosPage.filters.clearFilters()
+    await expect(todosPage.todoItem.filter({ hasText: 'low' })).toBeVisible()
+    await expect(todosPage.todoItem.filter({ hasText: 'medium' })).toBeVisible()
+    await expect(todosPage.todoItem.filter({ hasText: 'high' })).toBeVisible()
+  })
+})
